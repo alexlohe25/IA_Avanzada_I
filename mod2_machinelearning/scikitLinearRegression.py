e10f4447d6e32a990ce8ad4103d721c161b06047
Alejandro López <alexlohe25@gmail.com>
2022-09-13 14:51:45 -0500

    Report based fixes

--- mod2_machinelearning/scikitLinearRegression.py
+++ mod2_machinelearning/scikitLinearRegression.py
@@ -5,6 +5,7 @@
 #   Animes (TV, Películas y OVAs) empleados: 11985
 
 import numpy as np
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
@@ -56,33 +57,38 @@ Y_pred_train = regr.predict(X_train)
 Pred_error_train = Y_train - Y_pred_train
 
 print("MSE test: ",mean_squared_error(Y_test, Y_pred))
-print("Model score test: ", regr.score(X_test, Y_test))
-
 print("MSE train: ",mean_squared_error(Y_train, Y_pred_train))
-print("Model score train: ", regr.score(X_train, Y_train))
+
+print("Model score test: ", r2_score(Y_test, Y_pred))
+print("Model score train: ", r2_score(Y_train, Y_pred_train	))
+
+model_lasso = Lasso(alpha = 0.01)
+model_lasso.fit(X_train, Y_train)
+pred_train_lasso = model_lasso.predict(X_train)
+pred_test_lasso = model_lasso.predict(X_test)
+
+print("MSE in Lasso train: ", mean_squared_error(Y_train, pred_train_lasso))
+print("MSE in Lasso test: ", mean_squared_error(Y_test, pred_test_lasso))
+
+print("Lasso score train: ", r2_score(Y_train, pred_train_lasso))
+print("Lasso score test: ", r2_score(Y_test, pred_test_lasso))
 
 #plot
-figure, axis = plt.subplots(2,3)
+figure, axis = plt.subplots(2,2)
 #TEST
 #regresion
 axis[0,0].scatter(X_test, Y_test, alpha = 0.5)
 axis[0,0].plot(X_test, Y_pred, color='red', label = "MSE: " + str(mean_squared_error(Y_test, Y_pred)))
 axis[0,0].set_title("Anime Votes vs Rating (test data)")
 axis[0,0].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
-axis[0,0].set_ylim([0.5,5])
 axis[0,0].legend()
 
-#histograma(bias)
-axis[0,1].hist(Pred_error_test, alpha = 0.7, edgecolor = 'black', bins = 100)
-axis[0,1].set_title('Histogram of test prediction error')
-axis[0,1].set(xlabel = 'Rating prediction error (Y_test - Y_pred)', ylabel = 'Frequency')
-
-#varianza(?)
-axis[0,2].scatter(X_test, Y_test, alpha = 0.3, label = 'Real data')
-axis[0,2].scatter(X_test, Pred_error_test, color='orange',alpha = 0.1, label = 'Predicted data')
-axis[0,2].set_title("Real test data vs Predicted test data")
-axis[0,2].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
-axis[0,2].legend()
+#varianza
+axis[0,1].scatter(range(len(X_test)), Y_test, alpha = 0.3, label = 'Real data')
+axis[0,1].scatter(range(len(X_test)), Y_pred, color='orange',alpha = 0.1, label = 'Predicted data')
+axis[0,1].set_title("Real test data vs Predicted test data")
+axis[0,1].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
+axis[0,1].legend()
 
 #TRAIN
 #regresion
@@ -90,29 +96,13 @@ axis[1,0].scatter(X_train, Y_train, alpha = 0.3)
 axis[1,0].plot(X_train, Y_pred_train, color='red',label = "MSE: " + str(mean_squared_error(Y_train, Y_pred_train)))
 axis[1,0].set_title("Anime Votes vs Rating (train data)")
 axis[1,0].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
-axis[1,0].set_ylim([0.5,5])
 axis[1,0].legend()
 
-#histograma(bias)
-axis[1,1].hist(Pred_error_train, alpha = 0.7,edgecolor = 'black', bins = 100)
-axis[1,1].set_title('Histogram of train prediction error')
-axis[1,1].set(xlabel = 'Rating prediction error (Y_train - Y_pred_train)', ylabel = 'Frequency')
-
-#varianza(?)
-axis[1,2].scatter(X_train, Y_train, alpha = 0.3, label = 'Real data')
-axis[1,2].scatter(X_train, Pred_error_train, color='orange',alpha = 0.1, label = 'Predicted data')
-axis[1,2].set_title("Real train data vs Predicted train data")
-axis[1,2].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
-axis[1,2].legend()
+#varianza
+axis[1,1].scatter(range(len(X_train)), Y_train, alpha = 0.3, label = 'Real data')
+axis[1,1].scatter(range(len(X_train)), Y_pred_train, color='orange',alpha = 0.3, label = 'Predicted data')
+axis[1,1].set_title("Real train data vs Predicted train data")
+axis[1,1].set(xlabel = 'Votes in ln(x)', ylabel = 'Anime rating')
+axis[1,1].legend()
 plt.show()
 
-model_lasso = Lasso(alpha = 0.01)
-model_lasso.fit(X_train, Y_train)
-pred_train_lasso = model_lasso.predict(X_train)
-
-print("MSE in Lasso train: ", mean_squared_error(Y_train, pred_train_lasso))
-print("Lasso score train: ", r2_score(Y_train, pred_train_lasso))
-
-pred_test_lasso = model_lasso.predict(X_test)
-print("MSE in Lasso test: ", mean_squared_error(Y_test, pred_test_lasso))
-print("Lasso score test: ", r2_score(Y_test, pred_test_lasso))
